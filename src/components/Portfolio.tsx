fb9ec9723452c39689897f3bc457b333bde1097d
elanurbulbul <elanrbulbul@gmail.com>
2025-01-11 00:07:32 +0300

    EditedBuild

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import Image from 'next/image'
-import {motion} from "framer-motion"
-import project from "../assets/project.png"
-import proje1 from "../assets/proje1.png"
-import proje2 from "../assets/proje2.png"
+// import {motion} from "framer-motion"
+// import project from "../assets/project.png"
+// import proje1 from "../assets/proje1.png"
+// import proje2 from "../assets/proje2.png"
 
-const projects = [
-    {
-        title:"Healhy Nutrition Map",
-        desc:"A React app for diet planning and healthy recipe discovery, enabling users to explore recipes and plan meals with integrated API data sources.",
-        devStack:"React.js, Bootstrap, CSS, Fetch API, Axios",
-        link:"https://healthy-nutrition-map.vercel.app/",
-        gitHub:"https://github.com/elanurbulbul/HealthyNutritionMap",
-        src:proje1
-    },
-    {
-        title:"Film Folio",
-        desc:"A platform for discovering movies and TV shows, featuring personalized watchlists and detailed actor filmographies for an engaging user experience.",
-        devStack:"React.js, Bootstrap, Context API, Axios, Chakra UI, Vite",
-        link:"https://film-folio-eight.vercel.app/",
-        gitHub:"https://github.com/elanurbulbul/FilmFolio",
-        src:proje2
-    },
-    {
-        title:"Portfolio Website",
-        desc:"Portfolio site where I showcase my expertise in developing modern and user-friendly web applications. Learn more about my projects and technical skills.",
-        devStack:"Next.js, Tailwind, Framer Motion",
-        link:"https://film-folio-eight.vercel.app/",
-        gitHub:"https://github.com/elanurbulbul/MyPortfolio",
-        src:project
-    },
+// const projects = [
+//     {
+//         title:"Healhy Nutrition Map",
+//         desc:"A React app for diet planning and healthy recipe discovery, enabling users to explore recipes and plan meals with integrated API data sources.",
+//         devStack:"React.js, Bootstrap, CSS, Fetch API, Axios",
+//         link:"https://healthy-nutrition-map.vercel.app/",
+//         gitHub:"https://github.com/elanurbulbul/HealthyNutritionMap",
+//         src:proje1
+//     },
+//     {
+//         title:"Film Folio",
+//         desc:"A platform for discovering movies and TV shows, featuring personalized watchlists and detailed actor filmographies for an engaging user experience.",
+//         devStack:"React.js, Bootstrap, Context API, Axios, Chakra UI, Vite",
+//         link:"https://film-folio-eight.vercel.app/",
+//         gitHub:"https://github.com/elanurbulbul/FilmFolio",
+//         src:proje2
+//     },
+//     {
+//         title:"Portfolio Website",
+//         desc:"Portfolio site where I showcase my expertise in developing modern and user-friendly web applications. Learn more about my projects and technical skills.",
+//         devStack:"Next.js, Tailwind, Framer Motion",
+//         link:"https://film-folio-eight.vercel.app/",
+//         gitHub:"https://github.com/elanurbulbul/MyPortfolio",
+//         src:project
+//     },
 
-]
+// ]
 
 const Portfolio = () => {
   return (
