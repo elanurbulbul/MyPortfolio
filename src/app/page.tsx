e0685160b3452d10e622f0a38c318dcbf41cfd50
elanurbulbul <elanrbulbul@gmail.com>
2025-01-11 00:03:55 +0300

    CreatedPortfolioComponents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Portfolio/>
     </>
   );
 }
