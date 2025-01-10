df5d5e00549f8a4b12360de31afdb8caa7ed1377
elanurbulbul <elanrbulbul@gmail.com>
2025-01-11 02:49:47 +0300

    fixdProblem

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,8 +22,8 @@ const Contact = () => {
         </div>
         
         <div className='bg-white-10 p-6 rounded-xl max-w-[550px]'>
-            <h2 className='text-5xl font-bold text-orange-400 mb-4'>Let's connect</h2>
-            <p className='text-white/70 mb-6'> Send me a message and lets schedule a call!</p>
+        <h2 className='text-5xl font-bold text-orange-400 mb-4'>Let&apos;s connect</h2>
+        <p className='text-white/70 mb-6'> Send me a message and lets schedule a call!</p>
             <form className='space-y-4' action="https://getform.io/f/bejjprqa" method='POST'>
                 <div className='grid md:grid-cols-2 gap-4'>
                     <input type='text' name='name' className='bg-black/70 rounded-xl p-3 focus: outline-none focus:ring-2 focus:ring-orange-400' placeholder='First Name' />
