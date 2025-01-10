8ff2a72fc4626d592c8b2afcd4bfafc29743a8f7
elanurbulbul <elanrbulbul@gmail.com>
2025-01-11 00:14:31 +0300

    fixedProblem

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 // import {motion} from "framer-motion"
 // import project from "../assets/project.png"
 // import proje1 from "../assets/proje1.png"
