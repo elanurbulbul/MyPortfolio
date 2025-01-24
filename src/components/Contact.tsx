d9c854e2bd61c910729f222a4687fbf108db3970
elanurbulbul <elanrbulbul@gmail.com>
2025-01-24 19:27:28 +0300

    EditedCoontactComponent

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,28 +10,35 @@ const Contact = () => {
       className="max-w-[1400px]  grid grid-cols-12  items-center justify-between text-white/70 px-10 lg:px-16 xxl:px-0 py-16 lg:py-24"
       id="contact"
     >
-      <div className="col-span-12 lg:col-span-4 flex flex-col sm:flex-row lg:flex-col items-center lg:items-start justify-evenly lg:space-y-8 mb-8 lg:mb-0">
-        <div className="flex flex-row items-center mb-2 sm:mb-0 lg:mb-4">
-          <Image src={phone} alt="phone" className="h-[70px] md:h-[90px] xl:h-[110px] w-auto mr-3 md:mr-1 xl:mr-4" />
+      <div className="col-span-12 lg:col-span-4 flex flex-col items-center h-100  justify-between">
+        <div>
+        <h2 className="lg:text-start text-center font-semibold font-sans text-orange-400 ">
+          Let&apos;s connect
+        </h2>
+        <p className="text-white/70 mb-2 lg:text-start text-center">
+          Send me a mail and let&apos;s schedule a call!
+        </p>
+        </div>
+     <div className=" flex flex-col sm:flex-row lg:flex-col items-center lg:items-start justify-evenly space-x-0 md:space-x-4 lg:space-x-0 lg:space-y-2 lg:mb-0">
+     <div className="flex flex-row items-center ">
+          <Image src={phone} alt="phone" className="h-[70px] md:h-[90px] w-auto mr-3 md:mr-1 xl:mr-4" />
           <a href="tel:+905348557050">
             <p className="text-xl">0534 855 7050</p>
           </a>
         </div>
         <div className="flex flex-row items-center">
-          <Image src={mail} alt="mail" className="h-[70px] md:h-[90px] xl:h-[110px]  w-auto mr-2 md:mr-0 xl:mr-4" />
+          <Image src={mail} alt="mail" className="h-[70px] md:h-[90px]   w-auto mr-2 md:mr-0 xl:mr-4" />
           <a href="mailto:elanrbulbul@gmail.com">
             <p className="text-xl">elanrbulbul@gmail.com</p>
           </a>
         </div>
+     </div>
+       
       </div>
 
-      <div className="col-span-12 lg:col-span-8 bg-slate-900 p-6 rounded-xl">
-        <h2 className="lg:text-start text-center font-semibold font-sans text-orange-400 mb-8">
-          Let&apos;s connect
-        </h2>
-        <p className="text-white/70 mb-6 lg:text-start text-center">
-          Send me a message and let&apos;s schedule a call!
-        </p>
+      <div className="col-span-12 lg:col-span-8 bg-slate-900 p-6 py-8 rounded-xl">
+        
+        
         <form
           className="space-y-4"
           action="https://getform.io/f/bejjprqa"
